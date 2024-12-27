d20890f1e4041c344e8ab9d61aa2a3a5e9279f34
veerapareddy dinesh <veerapareddydinesh@gmail.com>
2024-12-27 20:29:33 +0530

    1

--- src/components/transferdetails.jsx
+++ src/components/transferdetails.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
-
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 const Transferdetails = () => {
   const [addr, setAddr] = useState(""); // Move useState to the top level
-
+  const options = [
+    'documents', 'keys', 'luggages'
+  ];
+  const defaultOption = options[0];
   useEffect(() => {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(
@@ -41,6 +45,14 @@ const Transferdetails = () => {
       <h1>Send Items - Enter Exact Location</h1>
       <button onClick={() => window.location.reload()}>Location Detect</button>
       <p>Detected Address: {addr || "Detecting location..."}</p>
+      <p>Is this location correct or wrong</p>
+      <button>Submit</button>
+      {/* <input type="text" placeholder="select category" /> */}
+      <Dropdown options={options} value={defaultOption} placeholder="Select an option" />
+      <input type="text" placeholder="product name" />
+      <input type="text" placeholder="Weight" />
+      <button>Find a Driver</button>
+      
     </div>
   );
 };
